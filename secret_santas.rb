e85135276c8acf0a1aa863abe13eb4f49d74cd9f
Ryan Graham <ryangrahamwpb@gmail.com>
2015-09-15 20:06:54 -0400

    Person tests pass

--- secret_santas.rb
+++ secret_santas.rb
@@ -1,37 +1,38 @@
 require "minitest/autorun"
 
 class Person
-  attr_reader :line
+  attr_reader :first_name, :last_name, :email
 
   def initialize(line)
-    @line = line
+    match = /(\S+)\s+(\S+)\s+<(.*)>/.match(line)
+    @first_name = match[1]
+    @last_name = match[2]
+    @email = match[3]
+
+
   end
 
-  def first_name
-    line.split.first
-  end
+
+
 end
 
 class TestPerson < Minitest::Test
   def setup
-    @people = [
-      "Luke Skywalker <luke@theforce.net>",
-      "Leia Skywalker <leia@therebellion.org>",
-      "Toula Portokalos <toula@manhunter.org>",
-      "Gus Portokalos <gus@weareallfruit.net>",
-      "Bruce Wayne <bruce@imbatman.com>",
-      "Virgil Brigman <virgil@rigworkersunion.org>",
-      "Lindsey Brigman <lindsey@iseealiens.net>"
-    ]
+    @person = "Luke Skywalker <luke@theforce.net>"
   end
 
   def test_initializes_with_first_name
-    person = Person.new(@people.first)
+    person = Person.new(@person)
     assert_equal "Luke", person.first_name
   end
 
   def test_initializes_with_last_name
-    person = Person.new(@people.first)
+    person = Person.new(@person)
     assert_equal "Skywalker", person.last_name
+  end
+
+  def test_initializes_with_email
+    person = Person.new(@person)
+    assert_equal "luke@theforce.net", person.email
   end
 end
