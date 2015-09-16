cb46c461a1c5a6d94f147f3979c1835dab174078
TJ Stankus <tjstankus@gmail.com>
2015-09-15 20:18:24 -0400

    Add test for invalid line, cleanup whitespace, refactor tests

--- secret_santas.rb
+++ secret_santas.rb
@@ -8,31 +8,27 @@ class Person
     @first_name = match[1]
     @last_name = match[2]
     @email = match[3]
-
-
   end
-
-
-
 end
 
 class TestPerson < Minitest::Test
   def setup
-    @person = "Luke Skywalker <luke@theforce.net>"
+    @person = Person.new("Luke Skywalker <luke@theforce.net>")
   end
 
   def test_initializes_with_first_name
-    person = Person.new(@person)
-    assert_equal "Luke", person.first_name
+    assert_equal "Luke", @person.first_name
   end
 
   def test_initializes_with_last_name
-    person = Person.new(@person)
-    assert_equal "Skywalker", person.last_name
+    assert_equal "Skywalker", @person.last_name
   end
 
   def test_initializes_with_email
-    person = Person.new(@person)
-    assert_equal "luke@theforce.net", person.email
+    assert_equal "luke@theforce.net", @person.email
+  end
+
+  def test_raises_error_for_invalid_line
+    assert_raises { Person.new("Person") }
   end
 end
